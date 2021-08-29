cfe7d2953d06d568ff309ea8c733a2b2235282af
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-08-29 23:24:40 +0900

    Updates

---
+++ precache-manifest.5c3b0f9e8381bce0191cd820245a26e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeaeda9cb452d8777c601f17b061f0de",
+    "url": "/witter/index.html"
+  },
+  {
+    "revision": "42d889a1ec1aed47bc6d",
+    "url": "/witter/static/css/main.51c00765.chunk.css"
+  },
+  {
+    "revision": "02f632a46cc828ce352b",
+    "url": "/witter/static/js/2.e1850d14.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/witter/static/js/2.e1850d14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d889a1ec1aed47bc6d",
+    "url": "/witter/static/js/main.a0d9b9f8.chunk.js"
+  },
+  {
+    "revision": "4c15ee2dde8fc72357db",
+    "url": "/witter/static/js/runtime-main.947571d5.js"
+  }
+]);
